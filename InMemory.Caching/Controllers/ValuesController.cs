c96a7391e31c416eba1d1f570cfbd570cf3e32ae
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2024-02-21 07:09:27 +0300

    Add project files.

---
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        //[HttpGet("set/{name}")]
+        //public void SetName(string name)
+        //{
+        //    _memoryCache.Set("name", name);
+
+
+        //}
+
+        //[HttpGet]
+        //public string GetName()
+        //{
+
+        //    if (_memoryCache.TryGetValue<string>("name", out string name))
+        //    {
+        //        return name.Substring(2);
+        //    }
+
+        //    return "";
+        //}
+        [HttpGet("setDate")]
+        public void SetDate()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+
+            });
+        }
+        [HttpGet("getDAte")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+    }
+}
